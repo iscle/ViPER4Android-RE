19c964142d2a9d71233d7a45e16205a66ee29ebd
Iscle <albertiscle9@gmail.com>
2023-03-01 19:53:22 +0100

    Fix merge

--- app/src/main/java/com/aam/viper4android/ViPERApp.kt
+++ app/src/main/java/com/aam/viper4android/ViPERApp.kt
@@ -5,10 +5,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
-import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ViPERApp : Application() {
