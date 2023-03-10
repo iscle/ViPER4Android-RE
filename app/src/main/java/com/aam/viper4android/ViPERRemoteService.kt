addcb08bc4677ac780307977bcb0f3766e0edb54
Iscle <albertiscle9@gmail.com>
2023-03-10 20:39:29 +0100

    Initial remote service structure

---
+++ app/src/main/java/com/aam/viper4android/ViPERRemoteService.kt
@@ -0,0 +1,51 @@
+package com.aam.viper4android
+
+import android.app.Service
+import android.content.Intent
+import android.os.*
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ViPERRemoteService : Service() {
+    lateinit var viperManager: ViPERManager
+    private var messenger: Messenger? = null
+
+    private class IncomingHandler(private val viperManager: ViPERManager) : Handler(Looper.getMainLooper()) {
+        private val TAG = "ViPERRemoteService"
+        private val MSG_GET_PARAMETER = 1
+        private val MSG_GET_PARAMETER_EFFECT_ANALOGX = 1
+        private val MSG_GET_PARAMETER_EFFECT_ANALOGX_ENABLED = 1
+
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                MSG_GET_PARAMETER -> handleGetParameter(msg, msg.arg1, msg.arg2)
+                else -> super.handleMessage(msg)
+            }
+        }
+
+        private fun handleGetParameter(msg: Message, effect: Int, parameter: Int) {
+            when (effect) {
+                MSG_GET_PARAMETER_EFFECT_ANALOGX -> {
+                    when (parameter) {
+                        MSG_GET_PARAMETER_EFFECT_ANALOGX_ENABLED -> {
+
+                        }
+                        else -> {
+                            Log.e(TAG, "handleGetParameter: Unknown AnalogX parameter: $parameter")
+                        }
+                    }
+                }
+                else -> {
+                    Log.e(TAG, "handleGetParameter: Unknown effect: $effect")
+                }
+            }
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder {
+        return messenger.let {
+            it ?: Messenger(IncomingHandler(viperManager)).also { messenger = it }
+        }.binder
+    }
+}
